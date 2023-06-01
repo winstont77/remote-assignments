66a612f5bc2cae75832019e073c80d5c0e2b6209
winstont77 <45140188+winstont77@users.noreply.github.com>
2023-06-01 16:25:31 +0800

    Assignment 1: Function and Array

--- Week-2/Assignment-1/app.js
+++ Week-2/Assignment-1/app.js
@@ -25,3 +25,40 @@ console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
 console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print2 (the first position)
 console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
 
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.querySelectorAll('pre code').forEach((block) => {
+//       hljs.highlightBlock(block);
+//     });
+//   });
+
+document.querySelector('code').innerHTML=`function max(numbers) {
+    let max=numbers[0];
+    for(let i=1;i&ltnumbers.length;i++){
+        if(max&ltnumbers[i]){
+            max=numbers[i]
+        }
+    }
+    return max;
+}
+
+function findPosition(numbers, target) {
+    for(let i=0;i&ltnumbers.length;i++){
+        if(numbers[i]===target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([5, 2, 7, 1, 6])); // should print 7
+console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
+console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
+console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print2 (the first position)
+console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1`
+
+
+document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+});
