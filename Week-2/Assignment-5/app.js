a206cfefc0cf7e0df280fd1672a8856487d2f09a
winstont77 <45140188+winstont77@users.noreply.github.com>
2023-06-01 16:46:09 +0800

    Assignment 5: Algorithm Practice

--- Week-2/Assignment-5/app.js
+++ Week-2/Assignment-5/app.js
@@ -18,3 +18,30 @@ function binarySearchPosition(numbers, target) {
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // Output: 0
 
 console.log(binarySearchPosition([1, 2 ,5, 6, 7], 6)); // shouldprint 3
+
+
+
+document.querySelector('code').innerHTML=`function binarySearchPosition(numbers, target) {
+  let start = 0;
+  let end = numbers.length - 1;
+  let mid = Math.floor((start+end)/2);
+  while(start !== mid || end !== mid){
+    if(target&gtnumbers[mid]){
+      start = mid+1;
+      mid = Math.floor((start+end)/2);
+    }else if(target&ltnumbers[mid]){
+      end = mid -1;
+      mid = Math.floor(((start+end)/2));
+    }else{
+      return mid
+    }
+  }
+}
+
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // Output: 0
+
+console.log(binarySearchPosition([1, 2 ,5, 6, 7], 6)); // shouldprint 3`
+
+document.querySelectorAll('pre code').forEach((block) => {
+  hljs.highlightBlock(block);
+});
