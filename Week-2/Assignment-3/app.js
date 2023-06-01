c2ce186b894a5009089a28ea790ad387ed6c048f
winstont77 <45140188+winstont77@users.noreply.github.com>
2023-06-01 16:37:19 +0800

    Assignment 3: Data Manipulation

--- Week-2/Assignment-3/app.js
+++ Week-2/Assignment-3/app.js
@@ -3,7 +3,7 @@ function groupByKey(input) {
   
     input.map(({ key, value }) => {
       grouped[key] = (grouped[key] || 0) + value;
-      console.log(grouped[key])
+      // console.log(grouped[key])
     });
   
     return grouped;
@@ -18,3 +18,31 @@ function groupByKey(input) {
   ];
   
   console.log(groupByKey(input2));
+
+
+document.querySelector('code').innerHTML=`function groupByKey(input) {
+  let grouped = {};
+
+  input.map(({ key, value }) =&gt {
+    grouped[key] = (grouped[key] || 0) + value;
+    //console.log(grouped[key])
+  });
+
+  return grouped;
+}
+
+let input2 = [
+  { key: "a", value: 3 },
+  { key: "b", value: 1 },
+  { key: "c", value: 2 },
+  { key: "a", value: 3 },
+  { key: "c", value: 5 },
+];
+
+console.log(groupByKey(input2));
+// should print {a:6, b:1, c:7}
+`
+
+document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+});
