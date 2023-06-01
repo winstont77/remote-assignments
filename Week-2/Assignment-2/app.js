96df6292363f8ea88449dd30e93437c14250b44b
winstont77 <45140188+winstont77@users.noreply.github.com>
2023-06-01 16:31:51 +0800

    Assignment 2: Function, Array, and Object

--- Week-2/Assignment-2/app.js
+++ Week-2/Assignment-2/app.js
@@ -25,3 +25,35 @@ console.log(
         ],
     })
 ); // should print the average price of all products
+
+document.querySelector('code').innerHTML=`function avg(data) {
+    let total=0;
+    for(let i=0;i&ltdata.products.length;i++){
+        total +=data.products[i].price;
+    }
+    return total/data.products.length;
+}
+
+console.log(
+    avg({
+        size: 3,
+        products: [
+            {
+                name: 'Product 1',
+                price: 100,
+            },
+            {
+                name: 'Product 2',
+                price: 700,
+            },
+            {
+                name: 'Product 3',
+                price: 250,
+            },
+        ],
+    })
+); // should print the average price of all products`
+
+document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+});
