b2bcc8bb6a062b8361df88dce5e9cf92ebbe9065
winstont77 <45140188+winstont77@users.noreply.github.com>
2023-06-02 11:34:59 +0800

    Assignment 3: Data Manipulation

--- Week-2/Assignment-3/app.js
+++ Week-2/Assignment-3/app.js
@@ -1,26 +1,56 @@
+function count(input) {
+  let obj = {};
+  input.map(index=>{
+    obj[index] = (obj[index]||0) + 1;
+  })
+  return obj
+}
+
+let input1 = ["a", "b", "c", "a", "c", "a", "x"];
+console.log(count(input1));
+// should print {a:3, b:1, c:2, x:1}
+
+
+
 function groupByKey(input) {
-    let grouped = {};
+  let grouped = {};
+
+  input.map(({ key, value }) => {
+    grouped[key] = (grouped[key] || 0) + value;
+    // console.log(grouped[key])
+  });
+
+  return grouped;
+}
   
-    input.map(({ key, value }) => {
-      grouped[key] = (grouped[key] || 0) + value;
-      // console.log(grouped[key])
-    });
-  
-    return grouped;
-  }
-  
-  let input2 = [
-    { key: "a", value: 3 },
-    { key: "b", value: 1 },
-    { key: "c", value: 2 },
-    { key: "a", value: 3 },
-    { key: "c", value: 5 },
-  ];
-  
-  console.log(groupByKey(input2));
+let input2 = [
+  { key: "a", value: 3 },
+  { key: "b", value: 1 },
+  { key: "c", value: 2 },
+  { key: "a", value: 3 },
+  { key: "c", value: 5 },
+];
+
+console.log(groupByKey(input2));
 
 
-document.querySelector('code').innerHTML=`function groupByKey(input) {
+document.querySelector('code').innerHTML=`
+function count(input) {
+  let obj = {};
+  input.map(index=&lt{
+    obj[index] = (obj[index]||0) + 1;
+  })
+  return obj
+}
+
+let input1 = ["a", "b", "c", "a", "c", "a", "x"];
+console.log(count(input1));
+// should print {a:3, b:1, c:2, x:1}
+
+
+
+
+function groupByKey(input) {
   let grouped = {};
 
   input.map(({ key, value }) =&gt {
