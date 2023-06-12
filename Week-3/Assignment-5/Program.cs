f42ecf407efdef728cb92aff6c422103074af48d
winstont77 <winstont77@gmail.com>
2023-06-12 15:54:49 +0800

    Assignment 5: Algorithm Practice (Advanced Optional)
    Given an array of integers, return indices of the two numbers such that they add up to a
    specific target. You may assume that each input would have exactly one solution, and you
    may not use the same element twice.

---
+++ Week-3/Assignment-5/Program.cs
@@ -0,0 +1,35 @@
+﻿static int[] TwoSum(int[] nums, int target)
+{
+    
+    Dictionary<int, int> complements = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int complement = target - nums[i];
+
+        
+        if (complements.ContainsKey(complement))
+        {
+            
+            return new int[] { complements[complement], i };
+        }
+
+        
+        complements[nums[i]] = i;
+    }
+
+    return null;
+}
+
+int[] nums = { 2, 7, 11, 15 };
+int target = 9;
+
+int[] result = TwoSum(nums, target);
+if (result != null)
+{
+    Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+}
+else
+{
+    Console.WriteLine("No solution found.");
+}
