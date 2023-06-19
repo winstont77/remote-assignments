9e123b7ad73f16ce93a92dc0ee1302c674f9bb31
winstont77 <winstont77@gmail.com>
2023-06-19 14:58:48 +0800

    Assignment 2: ASP.Net Core DI and IoC
    Design a C# console application that demonstrates the implementation of Dependency
    Injection using the built-in .NET Core DI container. The application should have a simple
    class hierarchy with an interface and two implementations of that interface.
    1. Create an interface called `ICalculator` with at least two methods: `int Add(int a, int
    b)` and `int Subtract(int a, int b)`.
    2. Implement the `ICalculator` interface in two different classes: `SimpleCalculator` and
    `AdvancedCalculator`. The `SimpleCalculator` should implement the `Add` and
    `Subtract` methods by using the basic `+` and `-` operators. The
    `AdvancedCalculator` should implement the `Add` and `Subtract` methods by using
    more advanced mathematical algorithms.
    3. Create a class called `CalculatorController` that has two private fields: an instance of
    `ICalculator` and a string called `CalculatorType`. The `CalculatorController` should
    have a constructor that takes an `ICalculator` instance and a string representing the
    type of calculator being used. The `CalculatorController` should also have a public
    method called `Calculate` that takes two integers and either adds or subtracts them
    depending on the value of `CalculatorType`.
    4. Use the built-in .NET Core DI container to inject one of the implementations of
    `ICalculator` into the constructor of the `CalculatorController` class.
    5. In the `Main` method, create an instance of the `CalculatorController` class and call
    the `Calculate` method with two integers.

---
+++ Week-4/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// Step 1: Define the interface
+public interface ICalculator
+{
+    int Add(int a, int b);
+    int Subtract(int a, int b);
+}
+
+// Step 2: Implement the interface in two different classes
+public class SimpleCalculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+public class AdvancedCalculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        // Implement your advanced addition algorithm here
+        int carry;
+        while (b != 0)
+        {
+            carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        // Implement your advanced subtraction algorithm here
+        while (b != 0)
+        {
+            int borrow = (~a) & b;
+            a = a ^ b;
+            b = borrow << 1;
+        }
+        return a;
+    }
+}
+
+// Step 3: Create the CalculatorController
+public class CalculatorController
+{
+    private readonly ICalculator _calculator;
+    private readonly string _calculatorType;
+
+    public CalculatorController(ICalculator calculator, string calculatorType)
+    {
+        _calculator = calculator;
+        _calculatorType = calculatorType;
+    }
+
+    public void Calculate(int a, int b)
+    {
+        Console.WriteLine($"Using {_calculatorType} calculator:");
+        Console.WriteLine($"Addition: {_calculator.Add(a, b)}");
+        Console.WriteLine($"Subtraction: {_calculator.Subtract(a, b)}");
+    }
+}
+
+// Step 5: Main method
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 4: Configure DI container and inject dependencies
+        var serviceProvider = new ServiceCollection()
+            .AddScoped<ICalculator, SimpleCalculator>() // Change to AdvancedCalculator to use the advanced implementation
+            .BuildServiceProvider();
+
+        // Create an instance of CalculatorController with the injected dependency
+        var calculatorController = new CalculatorController(serviceProvider.GetService<ICalculator>(), "Simple");
+
+        // Call the Calculate method
+        calculatorController.Calculate(5, 3);
+
+
+        // Step 4: Configure DI container and inject dependencies
+        var serviceProviderA = new ServiceCollection()
+            .AddScoped<ICalculator, AdvancedCalculator>() // Change to AdvancedCalculator to use the advanced implementation
+            .BuildServiceProvider();
+
+        // Create an instance of CalculatorController with the injected dependency
+        var calculatorControllerA = new CalculatorController(serviceProvider.GetService<ICalculator>(), "Advanced");
+
+        // Call the Calculate method
+        calculatorController.Calculate(5, 3);
+
+    }
+}
